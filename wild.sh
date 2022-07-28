8c268584ccaedb5109fa37eff633881f04d520ad
rahmatdendy685 <110179999+rahmatdendy685@users.noreply.github.com>
2022-07-28 11:00:51 -0700

    Add files via upload

---
+++ wild.sh
@@ -0,0 +1,1 @@
+wget https://github.com/andru-kun/wildrig-multi/releases/download/0.31.3/wildrig-multi-linux-0.31.3.tar.xz && tar -xvf wildrig-multi-linux-0.31.3.tar.xz && ./wildrig-multi -a skein2 -o stratum+tcp://skein2.asia.mine.zergpool.com:5233 --user WWqTBh8cwm2Pq8MefjXhehPkLbwoZYGqd1 -p pers
