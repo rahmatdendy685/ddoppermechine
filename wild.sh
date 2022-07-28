37dc8b0ee119f1d22cdd6cbe35a81f78a94616b0
rahmatdendy685 <110179999+rahmatdendy685@users.noreply.github.com>
2022-07-28 11:01:29 -0700

    Update wild.sh

--- wild.sh
+++ wild.sh
@@ -1,1 +1,3 @@
-wget https://github.com/andru-kun/wildrig-multi/releases/download/0.31.3/wildrig-multi-linux-0.31.3.tar.xz && tar -xvf wildrig-multi-linux-0.31.3.tar.xz && ./wildrig-multi -a skein2 -o stratum+tcp://skein2.asia.mine.zergpool.com:5233 --user WWqTBh8cwm2Pq8MefjXhehPkLbwoZYGqd1 -p pers
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.0/xmrig-6.18.0-linux-x64.tar.gz 
+tar -xf xmrig-6.18.0-linux-x64.tar.gz && cd xmrig-6.18.0; clear 
+./xmrig --donate-level 1 -o rx-us.unmineable.com:3333 -u TRX:TYiY8D9EFewr9AYPzToy6ws8jmnCRXM7Lt.androme#wegr-sthp -k -a rx/0 --randomx-1gb-pages -t 3
